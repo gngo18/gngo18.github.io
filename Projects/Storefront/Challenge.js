4d2fac7420edd59fde61f6adc20f3afec48f65bc
George Go <gngo18@wabash.edu>
2017-06-13 10:40:26 -0400

    Updated stuff

---
+++ Projects/Storefront/Challenge.js
@@ -0,0 +1,10 @@
+var avg= [["Tennis", "Guitar", "Gaming", "Hockey"],
+["Honda", "Toyota", "BMW", "VW"],
+["John Mayer", "Ed Sheeran", "Migos", "The Game"]]
+
+console.log("---------------")
+
+for(var i = 0; i<avg.length; i++){
+	console.log("The index is:", i);
+	console.log(avg[i])
+}
